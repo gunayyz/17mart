81db59e1fd0fae8216f3090cc4ae4be1d55b4c9a
gunayyz <100687596+gunayyz@users.noreply.github.com>
2022-03-17 12:23:14 +0400

    Add files via upload

---
+++ hw1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace homework17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(MaxArr(10,-90,90,0,-1,20,-100));
+        }
+        static int MaxArr(params int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i]>max)
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+    }
+}
+

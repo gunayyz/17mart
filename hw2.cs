52c8cfcf73371989be83b4bbd164b136467b6a19
“gunayyz” <7cgq295@code.edu.az>
2022-03-17 12:04:03 +0400

    commit

---
+++ hw2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace hw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. Bir integer eded qebul edib, bu ededi binary edede 
+            //chevirende neche 1 oldugunu tapin. Mes: input 7(binary 
+            //chevirende 111 alinir).output 3.
+            
+            Console.WriteLine(BinaryConverter(7));
+        }
+        static int BinaryConverter(int a )
+        {
+            int count = 0;
+            while (a>0)
+            {
+                if (a % 2 == 1)
+                {
+                    count++;
+                    
+                }
+                a = a / 2;
+
+
+
+            }
+            return count;
+        }
+        
+    }
+
+}
